65e46351df63ce17391ef6e24b2a97eb8256da4f
Seyed Yahya Shirazi <shirazi@ieee.org>
2026-04-09 13:17:22 -0700

    fix: wire iOS APNS native bridge

--- packages/web/ios/App/App/AppDelegate.swift
+++ packages/web/ios/App/App/AppDelegate.swift
@@ -24,6 +24,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return ApplicationDelegateProxy.shared.application(application, continue: userActivity, restorationHandler: restorationHandler)
     }
 
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        NotificationCenter.default.post(
+            name: .capacitorDidRegisterForRemoteNotifications,
+            object: deviceToken
+        )
+    }
+
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        NotificationCenter.default.post(
+            name: .capacitorDidFailToRegisterForRemoteNotifications,
+            object: error
+        )
+    }
+
     // Keep WebSocket alive when app enters background.
     // iOS grants ~30 seconds of background execution time.
     // During this window, the WebSocket stays connected and can
